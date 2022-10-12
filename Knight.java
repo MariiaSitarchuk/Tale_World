cf3cc7a679c0508743285acf25f8a78e5f3451a8
MariiaSitarchuk <92577191+MariiaSitarchuk@users.noreply.github.com>
2022-10-12 18:31:35 +0300

    Create Knight.java

---
+++ Knight.java
@@ -0,0 +1,14 @@
+public class Knight extends Noble {
+    //Character
+    //Private static final Random randomGenerator = new Random();
+    public Knight() {    // constructor
+        super(2, 12);
+        //super(hp: randomGenerator.nextInt(bound:11) + 2, power: randomGenerator.nextInt(bound:11) + 2);
+    }
+
+    //@Override
+    //public void kick(Character c){
+    //    c.setHP(c.getHP() - randomGenerator.nextInt(getPower());
+    //}
+}
+
