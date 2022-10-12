bba1b0efa671f4e7c7dce7aeeb555cc0f5fe21e5
MariiaSitarchuk <92577191+MariiaSitarchuk@users.noreply.github.com>
2022-10-12 18:22:07 +0300

    Create Elf.java

---
+++ Elf.java
@@ -0,0 +1,17 @@
+import lombok.Override;
+// check this, not sure if this is right
+
+public class Elf extends Character {
+    public Elf() {    // constructor
+        super(hp: 10, power: 10);
+    }
+    @Override
+    public void kick(Character c){
+        if (c.power < this.power) {
+            c.hp = 0;
+        }
+        else {
+            c.power = 0;
+        }
+    }
+}
