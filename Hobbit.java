5ce1f25858423c6ad161766f2fc16ed30761f3db
MariiaSitarchuk <92577191+MariiaSitarchuk@users.noreply.github.com>
2022-10-12 18:19:14 +0300

    Create Hobbit.java

---
+++ Hobbit.java
@@ -0,0 +1,13 @@
+import lombok.Override;
+//need to check this...
+
+public class Hobbit extends Character {
+    public Hobbit() {    // constructor
+        super(hp: 3, power: 0);
+    }
+    @Override
+    public void kick(Character c){
+        System.out.println(“tears”);
+    }
+}
+
