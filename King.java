6bd9a926a97bf97116c344524420cba77fe341fb
MariiaSitarchuk <92577191+MariiaSitarchuk@users.noreply.github.com>
2022-10-12 18:30:31 +0300

    Create King.java

---
+++ King.java
@@ -0,0 +1,11 @@
+public class King extends Noble {
+    public King() {    // constructor
+        super(5, 15);
+        //super(hp: new Random().nextInt(bound:11) + 5, power: new Random().nextInt(bound:11) + 5);
+    }
+
+    //@Override
+    //public void kick(Character c){
+    //    c.setHP(c.getHP() - new Random().nextInt(getPower());
+    //}
+}
