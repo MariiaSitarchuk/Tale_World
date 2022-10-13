b0cb7dd18ac13e465c775d549ac185a3327c6bc6
MariiaSitarchuk <92577191+MariiaSitarchuk@users.noreply.github.com>
2022-10-13 14:02:38 +0300

    Update Noble.java

--- src/main/java/Noble.java
+++ src/main/java/Noble.java
@@ -1,15 +1,16 @@
-//import lombok.Override;
-// CHECK THIS TOO
+package java;
 
-public class Noble extends Character {
+import java.util.Random;
+
+public abstract class Noble extends Character {
     private static final Random randomGenerator = new Random();
 
     public Noble (int min, int max) {    // constructor
-        super(super(randomGenerator.nextInt(bound: max-min+1) + min, randomGenerator.nextInt(bound: max-min+1) + min);
+        super(randomGenerator.nextInt(max-min+1) + min, randomGenerator.nextInt(max-min+1) + min);
     }
 
     @Override
     public void kick(Character c){
-        c.setHP(c.getHP() - randomGenerator.nextInt(getPower());
+        c.setHP(c.getHP() - randomGenerator.nextInt(this.getPower());
     }
 }
