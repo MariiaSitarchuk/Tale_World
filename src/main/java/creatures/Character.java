96b3f9f917a52bb222c23c45da30ed2ebb2b4595
MariiaSitarchuk <92577191+MariiaSitarchuk@users.noreply.github.com>
2022-10-13 14:59:57 +0300

    Update Character.java

--- src/main/java/creatures/Character.java
+++ src/main/java/creatures/Character.java
@@ -9,13 +9,16 @@ import lombok.AllArgsConstructor;
 public abstract class Character { 
     @Getter @Setter
     private int power;//data member (also instance variable)
-    @Getter
     private int hp; //data member (also instance variable) 
  
 //void kick(Character c), boolean isAlive()
 
     public void setHP(int hp) {
         this.hp = Math.max(hp, 0);
+    }
+    
+    public void getHP() {
+        return this.hp;
     }
     
     public boolean isAlive() {
