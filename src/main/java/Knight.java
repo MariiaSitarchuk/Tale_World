1c7bf3a93dd4147ff2b366b94013af678651a22d
MariiaSitarchuk <92577191+MariiaSitarchuk@users.noreply.github.com>
2022-10-13 13:59:26 +0300

    Update Knight.java

--- src/main/java/Knight.java
+++ src/main/java/Knight.java
@@ -1,14 +1,8 @@
-public class Knight extends Noble {
-    //Character
-    //Private static final Random randomGenerator = new Random();
-    public Knight() {    // constructor
-        super(2, 12);
-        //super(hp: randomGenerator.nextInt(bound:11) + 2, power: randomGenerator.nextInt(bound:11) + 2);
-    }
+package java;
 
-    //@Override
-    //public void kick(Character c){
-    //    c.setHP(c.getHP() - randomGenerator.nextInt(getPower());
-    //}
+public class Knight extends Noble {
+    public Knight() {
+        super(2, 12);
+    }
 }
 
