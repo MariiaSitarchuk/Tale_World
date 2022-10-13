5973a704673007a75ba1e990559ccf6429cde683
MariiaSitarchuk <92577191+MariiaSitarchuk@users.noreply.github.com>
2022-10-13 13:37:24 +0300

    Update Noble.java

--- src/main/java/Noble.java
+++ src/main/java/Noble.java
@@ -5,7 +5,7 @@ public class Noble extends Character {
     private static final Random randomGenerator = new Random();
 
     public Noble (int min, int max) {    // constructor
-        super(super(hp: randomGenerator.nextInt(bound: max-min+1) + min, power: randomGenerator.nextInt(bound: max-min+1) + min);
+        super(super(randomGenerator.nextInt(bound: max-min+1) + min, randomGenerator.nextInt(bound: max-min+1) + min);
     }
 
     @Override
