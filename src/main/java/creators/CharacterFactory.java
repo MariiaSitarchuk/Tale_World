c19f7fa01d92b50344b7e0b2aa1c41a8f995ed28
MariiaSitarchuk <92577191+MariiaSitarchuk@users.noreply.github.com>
2022-10-13 14:07:40 +0300

    Update CharacterFactory.java

--- src/main/java/creators/CharacterFactory.java
+++ src/main/java/creators/CharacterFactory.java
@@ -1,14 +1,26 @@
-//add reflections (cms)
+package creators;
+
+import creatures.Noble;
+import creatures.Character;
+import lombok.SneakyThrows;
+import org.reflections.Reflections;
+
+import java.util.Random;
+import java.util.Set;
+
+import static org.reflections.scanners.Scanners.SubTypes;
+
 public class CharacterFactory {
-    
+
     @SneakyThrows
-    public static Character createCharacter() {
-        Reflections reflections = new Reflections(“src”);
+    public static Character createCharacter(){
+        Reflections reflections = new Reflections("characters");
 
         Set<Class<?>> subTypes = reflections.get(SubTypes.of(Character.class).asClass());
+
+        subTypes.remove(Noble.class);
 
         Class cls = (Class) subTypes.toArray()[new Random().nextInt(subTypes.size())];
         return (Character) cls.getDeclaredConstructor().newInstance();
     }
-
 }
